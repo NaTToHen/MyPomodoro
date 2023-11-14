42c57e9f414d79300b1cd737cd03674f591473be
Henrique Bonatto <nattohen@gmail.com>
2023-11-14 11:22:35 -0300

    Initial commit

---
+++ app/src/main/java/com/henbonattodev/mypomodoro/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.henbonattodev.mypomodoro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
